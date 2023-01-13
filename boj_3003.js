2b3024f76589edf01f1a26f754b72ce56569439b
frankeykr <frankeykr@gmail.com>
2023-01-13 17:16:09 +0900

    코멘트 수정

--- boj_3003.js
+++ boj_3003.js
@@ -1,4 +1,3 @@
-// 아직 이해가 안됨
 // https://overcome-the-limits.tistory.com/entry/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-%EB%B0%B1%EC%A4%80-3003-%ED%82%B9-%ED%80%B8-%EB%A3%A9-%EB%B9%84%EC%88%8D-%EB%82%98%EC%9D%B4%ED%8A%B8-%ED%8F%B0-with-nodejs
 const fs = require('fs');
 const input = fs.readFileSync('example.txt').toString().trim().split(' ').map(Number);
