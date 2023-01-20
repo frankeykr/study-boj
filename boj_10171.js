64a8be22e05576049386e1b9507cf431b3d90ba4
frankeykr <frankeykr@gmail.com>
2023-01-20 18:05:16 +0900

    참고 링크 추가

--- boj_10171.js
+++ boj_10171.js
@@ -1,3 +1,5 @@
+// 참고링크
+// https://stackoverflow.com/questions/21672334/javascript-how-to-show-escape-characters-in-a-string
 // 역슬래쉬 이스케이프 처리가 필요했다
 console.log("\\    /\\");
 console.log(" )  ( ')");
