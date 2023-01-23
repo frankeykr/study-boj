6ef3af8bfcc57a380f10c6cc81798cbc85c0bad9
frankeykr <frankeykr@gmail.com>
2023-01-23 15:21:50 +0900

    25083번 문제풀이 추가

---
+++ boj_25083.js
@@ -0,0 +1,7 @@
+// 역슬래쉬, 따옴표 이스케이프 처리가 필요했다
+console.log(`         ,r'"7`);
+console.log("r`-_   ,'  ,/");
+console.log(" \\\. \". L_r'");
+console.log("   `~\\/");
+console.log("      |");
+console.log("      |");
