3580f01452d4e30911fbfb02cda98de2db415389
frankeykr <frankeykr@gmail.com>
2023-02-16 08:32:19 +0900

    11022번 문제풀이 추가

---
+++ boj_11022.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const input = fs.readFileSync('example.txt').toString().split('\n');
+const count = Number(input[0]);
+
+for (let i=1; i<=count; i++) {
+  let numbers = input[i].split(' ');
+  let answer = Number(numbers[0])+Number(numbers[1]);
+  console.log(`Case #${i}: ${numbers[0]} + ${numbers[1]} = ${answer}`);
+}
